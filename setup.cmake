35788bd7dd9876f2301aacca36c3329bee951572
alpertunga-bile <kingdom19online@hotmail.com>
2024-08-02 17:08:06 +0300

    setup.cmake is updated

--- setup.cmake
+++ setup.cmake
@@ -30,10 +30,26 @@ function(enable_avx2)
     endif()
 endfunction(enable_avx2)
 
+function(enable_sse4_2)
+    # from glm repository's CMakeLists.txt
+    if((CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
+        add_compile_options(-msse4.2)
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+        add_compile_options(/QxSSE4.2)
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+        add_compile_options(/arch:SSE2)
+    endif()
+endfunction(enable_sse4_2)
+
 function(enable_iwyu)
     if(NOT WIN32)
         set(IWYU_ARGS include-what-you-use -w -Xiwyu --verbose=7)
         add_compile_definitions(CMAKE_CXX_INCLUDE_WHAT_YOU_USE=${IWYU_ARGS})
     endif(NOT WIN32)
 endfunction(enable_iwyu)
+
+function(is_main_project value)
+    set(value (${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR}))
+endfunction(is_main_project)
+
             
