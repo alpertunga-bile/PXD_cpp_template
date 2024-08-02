23b9ef3144aee7c401cfc6006e682c3d15b841f4
alpertunga-bile <kingdom19online@hotmail.com>
2024-08-02 11:16:13 +0300

    setup exec commands are added

---
+++ setup.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo "Setting the environment"
+python setup.py
+
+echo "Setting third party Github repositories"
+
+git submodule init
+
+git submodule update --depth 1
+
+echo "Going to PXD-STL directory"
+
+cd third-party\\PXD-STL
+
+echo "Setting PXD-STL repository"
+
+git submodule init
+
+echo "Updating PXD-STL repository's third-party repositories"
+
+git submodule update --depth 1
+
+clear
+
+echo "Setup is finished"
