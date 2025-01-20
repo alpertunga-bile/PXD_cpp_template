59e5b5bb681e03faf181d42caed2850e72867a62
alpertunga-bile <kingdom19online@hotmail.com>
2025-01-20 20:13:53 +0300

    not relevant files are removed

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include "third-party/PXD-STL/includes/logger.hpp"
-#include "third-party/PXD-STL/includes/random_gen.hpp"
+#include <iostream>
 
-auto main() -> int {
-  PXD_LOG_INFO("TEST");
-
-  for (int i = 0; i < 10; ++i) {
-    printf("%f\n", pxd::random::random_value<double>(0.0, 1.0));
-  }
+auto
+main() -> int
+{
+  std::cout << "Hello World\n";
 
   return 0;
 }
